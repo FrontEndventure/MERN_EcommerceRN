a2aa1ce6f886d091da43ec381843b7fe8332bd86
moohat <refixzcorp@gmail.com>
2022-06-14 08:39:01 -0700

    create post

--- BE/app.js
+++ BE/app.js
@@ -1,18 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv/config");
+require('dotenv/config');
 const port = 3000;
 // console.log(process.env);
 const api = process.env.API_URL;
 
+//midleware
+app.use(express.json());
+
 // http://localhost:3000/api/v1/products
 app.get(`${api}/products`, (req, res) => {
-  res.send(
-    "Hello World!, Taufik anda adalah orang hebat, kamu bisa pasti jadi React Native Developer Expert"
-  );
+  const product = {
+    id: 1,
+    name: 'hair dress',
+    image: 'some_url',
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-  console.log("panggil dari env:", api);
+  console.log('panggil dari env:', api);
 });
