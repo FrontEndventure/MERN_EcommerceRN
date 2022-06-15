90ce560385d1460bf821bf6046b76c6b78495a0d
moohat <refixzcorp@gmail.com>
2022-06-15 09:38:48 -0700

    configure mongodb and mongoose, test api

--- BE/app.js
+++ BE/app.js
@@ -4,27 +4,65 @@ require('dotenv/config');
 const port = 3000;
 // console.log(process.env);
 const api = process.env.API_URL;
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //midleware
 app.use(express.json());
+app.use(morgan('tiny'));
+
+const productShema = mongoose.Schema({
+  name: String, // String is shorthand for {type: String}
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model('Product', productShema);
 
 // http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'hair dress',
-    image: 'some_url',
-  };
-  res.send(product);
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then(createdProduct => {
+      res.status(201).json(createdProduct);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+  // res.send(newProduct);
 });
 
+mongoose
+  .connect(process.env.CONNECTION_STRING_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database',
+  })
+  .then(() => {
+    console.log('Database connected SUCCESS');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  // console.log(`Example app listening on port ${port}`);
   console.log('panggil dari env:', api);
 });
