4547156add58ce3cfd69f6fa3e3017d6a661fdde
moohat <refixzcorp@gmail.com>
2022-07-04 08:19:25 -0700

    get featured products

--- BE/src/routers/products.js
+++ BE/src/routers/products.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {Category} from '../models/category';
 const router = express.Router();
+const monggose = require('mongoose');
 
 // http://localhost:3000/api/v1/products
 const {Product} = require('../models/product');
@@ -17,6 +18,9 @@ router.get(`/`, async (req, res) => {
 //get product by id
 router.get(`/:id`, async (req, res) => {
   const id = req.params.id;
+  if (!monggose.isValidObjectId(req.params.id)) {
+    res.status(400).send('invalid Product Id');
+  }
   const productList = await Product.findById(id).populate('category');
   if (!productList) {
     res.status(500).json({success: false, message: 'Product not found'});
@@ -68,6 +72,101 @@ router.post(`/`, async (req, res) => {
     res.status(500).json({success: false});
   }
   res.send(product);
+});
+
+router.put(`/:id`, async (req, res) => {
+  if (!monggose.isValidObjectId(req.params.id)) {
+    res.status(400).send('invalid Product Id');
+  }
+
+  const {
+    name,
+    description,
+    richDescription,
+    image,
+    brand,
+    price,
+    category,
+    countInStock,
+    rating,
+    numReviews,
+    isFeatured,
+  } = req.body;
+  Category.findById(category, function (err, data) {
+    if (err) {
+      res.status(400).send('invalid Category');
+    }
+  });
+  //pake destructuring variable
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      description,
+      richDescription,
+      image,
+      brand,
+      price,
+      category,
+      countInStock,
+      rating,
+      numReviews,
+      isFeatured,
+    },
+    {new: true},
+  );
+
+  if (!product) {
+    res.status(500).json({success: false});
+  }
+  res.send(product);
+});
+
+//api/v1/id
+router.delete('/:id', (req, res) => {
+  if (!monggose.isValidObjectId(req.params.id)) {
+    res.status(400).send('invalid Product Id');
+  } else {
+    Product.findByIdAndRemove(req.params.id)
+      .then(product => {
+        if (product) {
+          return res.status(200).json({
+            success: true,
+            message: 'product deleted',
+          });
+        } else {
+          return res
+            .status(404)
+            .json({success: false, message: 'product not found'});
+        }
+      })
+      .catch(err => {
+        return res.status(400).json({success: false, error: err});
+      });
+  }
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const productCount = await Product.countDocuments(count => count).clone();
+
+  if (!productCount) {
+    res.status(500).json({success: false});
+  }
+  res.send({
+    jumlahProduk: productCount,
+  });
+});
+
+router.get(`/get/featured/:count`, async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  // const products = await Product.find({ isFeatured: true }).limit(+count);
+  const products = await Product.find({ isFeatured: true }).limit(+count);
+
+  if (!products) {
+      res.status(500).json({ success: false });
+  }
+  res.send(products);
 });
 
 module.exports = router;
